f908a8721ebd47ab87b9dd483421dc4fe66c8866
CarlG96 <carl.gardiner1@hotmail.com>
2022-09-05 14:46:38 +0000

    feat: Create superuser account and genre modelaccessible by admin account

--- synthestoryapp/models.py
+++ synthestoryapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Genre(models.Model):
+    genre_title = models.CharField(max_length=20, unique=True)
+    genre_image = CloudinaryField('image', default='placeholder')
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['creation_date']
+
+    def __str__(self):
+        return self.genre_title
