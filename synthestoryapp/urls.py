8bca63e25c3bdc2814490ac8751b1e0033491bdb
CarlG96 <carl.gardiner1@hotmail.com>
2022-09-08 13:45:44 +0000

    feat: Build basic view to display random story starts to the screen

--- synthestoryapp/urls.py
+++ synthestoryapp/urls.py
@@ -1,7 +1,8 @@
 from . import views
-from .views import get_genre_page
+from .views import get_genre_page, get_genre_type_page
 from django.urls import path
 
 urlpatterns = [
     path('', get_genre_page, name="genre-page"),
+    path('genre-type/', get_genre_type_page, name="genre-type")
 ]
--- synthestoryapp/views.py
+++ synthestoryapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Genre
-
+from .models import Genre, StoryStart
+import random
 
 def get_genre_page(request):
     genres = Genre.objects.all()
@@ -8,3 +8,15 @@ def get_genre_page(request):
         'genres': genres
     }
     return render(request, 'genre-page.html', context)
+
+def get_genre_type_page(request):
+    story_starts = StoryStart.objects.all()
+    story_starts_list = list(story_starts)
+    rand_number = random.randrange(0, len(story_starts_list))
+    story_start = story_starts_list[rand_number]
+    context = {
+        'story_start': story_start
+    }
+
+
+    return render(request, 'genre-type.html', context)
