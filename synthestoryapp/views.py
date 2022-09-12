51f3fda32ca2182cd398c76632904806b15563d8
CarlG96 <carl.gardiner1@hotmail.com>
2022-09-12 14:24:58 +0000

    fix: Change placeholder attribute in form to value to allow it to beposted

--- synthestoryapp/views.py
+++ synthestoryapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Genre, StoryStart, StoryMiddle, StoryEnd
+from django.shortcuts import render, redirect
+from .models import Genre, StoryStart, StoryMiddle, StoryEnd, StoryIdea
 from .forms import StoryIdeaForm
 import random
 
@@ -42,6 +42,42 @@ def get_genre_type_page(request, id):
 
     return render(request, 'genre-type.html', context)
 
+def post_genre_type_page(request, id):
+
+    # creates a random sentence that relates to the currently selected genre
+    # can be redone with better code
+
+    story_starts = StoryStart.objects.all().filter(genre=id)
+    story_starts_list = list(story_starts)
+    rand_number = random.randrange(0, len(story_starts_list))
+    story_start = story_starts_list[rand_number]
+
+    story_middles = StoryMiddle.objects.all().filter(genre=id)
+    story_middles_list = list(story_middles)
+    rand_number_two = random.randrange(0, len(story_middles_list))
+    story_middle = story_middles_list[rand_number_two]
+
+    story_ends = StoryEnd.objects.all().filter(genre=id)
+    story_ends_list = list(story_ends)
+    rand_number_three = random.randrange(0, len(story_ends_list))
+    story_end = story_ends_list[rand_number_three]
+
+    story_idea_form = StoryIdeaForm(request.POST)
+
+    if story_idea_form.is_valid():
+        story_idea_form.save()
+        return redirect('get_my_stories_page', id = user.id)
+
+
+
+    context = {
+        'story_start': story_start,
+        'story_middle': story_middle,
+        'story_end': story_end,
+        'story_idea_form': StoryIdeaForm()
+    }
+
+    return render(request, 'genre-type.html', context)
 
 def get_my_stories_page(request, id):
 
