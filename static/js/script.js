58e0b61e6e75f4fcd8afdf6a87749e8180ac343b
CarlG96 <carl.gardiner1@hotmail.com>
2022-09-12 13:15:32 +0000

    fix: Add script block to base.html to place js at bottom of genre-type page

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', event => {
+    let storyStart = ` {{ story_start }} `;
+document.getElementById("id_story_text").setAttribute("placeholder", storyStart);
+})
+
