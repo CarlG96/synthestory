cea6a19a6e203e5526e68512bcfb6947d53efe5e
CarlG96 <carl.gardiner1@hotmail.com>
2022-09-15 15:20:50 +0000

    style: Change model form text inputs to textareas to allow for smaller device sizes to display story ideas

--- synthestoryapp/forms.py
+++ synthestoryapp/forms.py
@@ -1,11 +1,16 @@
 from .models import StoryIdea
 from django import forms
+from django.forms import Textarea
 
 class StoryIdeaForm(forms.ModelForm):
 
     class Meta:
         model = StoryIdea
         fields = ('title', 'story_text')
+        widgets = {
+            'title': Textarea(attrs={'rows':1, 'cols':20}),
+            'story_text': Textarea(attrs={'rows':8, 'cols':20}),
+        }
         labels = {
             'title': 'Title',
             'story_text': 'Story Idea'
