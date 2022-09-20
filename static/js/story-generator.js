34c89b8bef3656fd81890db7a2a657c6c4cd58b8
CarlG96 <carl.gardiner1@hotmail.com>
2022-09-20 17:24:28 +0000

    refactor: Begin refactor of views.py to make code cleaner and in line with PEP8guidelines

--- static/js/story-generator.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', event => {
-    console.log('working');
-    let storyStart = `{{ story_start }} {{ story_middle }} {{ story_end }}`;
-    console.log(storyStart);
-    document.getElementById("id_story_text").setAttribute("value", storyStart);
-})
--- synthestoryapp/views.py
+++ synthestoryapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Genre, StoryStart, StoryMiddle, StoryEnd, StoryIdea, User
+from synthestoryapp import models
 from .forms import StoryIdeaForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -11,7 +11,7 @@ def get_home_page(request):
 
 @login_required
 def get_genre_page(request):
-    genres = Genre.objects.all()
+    genres = models.Genre.objects.all()
     context = {
         'genres': genres
     }
@@ -23,10 +23,10 @@ def get_genre_type_page(request, id):
         title = request.POST.get('title')
         story_text = request.POST.get('story_text')
         messages.add_message(request, messages.SUCCESS, f'Successfully added story idea: {title}.')
-        StoryIdea.objects.create(title=title, story_text=story_text, user=request.user)
+        models.StoryIdea.objects.create(title=title, story_text=story_text, user=request.user)
 
         # repeated elsewhere
-        story_ideas = StoryIdea.objects.all().filter(user = request.user.id)
+        story_ideas = models.StoryIdea.objects.all().filter(user = request.user.id)
             
         context = {
             'story_ideas': story_ideas
@@ -36,17 +36,17 @@ def get_genre_type_page(request, id):
     # creates a random sentence that relates to the currently selected genre
     # can be redone with better code
 
-    story_starts = StoryStart.objects.all().filter(genre=id)
+    story_starts = models.StoryStart.objects.all().filter(genre=id)
     story_starts_list = list(story_starts)
     rand_number = random.randrange(0, len(story_starts_list))
     story_start = story_starts_list[rand_number]
 
-    story_middles = StoryMiddle.objects.all().filter(genre=id)
+    story_middles = models.StoryMiddle.objects.all().filter(genre=id)
     story_middles_list = list(story_middles)
     rand_number_two = random.randrange(0, len(story_middles_list))
     story_middle = story_middles_list[rand_number_two]
 
-    story_ends = StoryEnd.objects.all().filter(genre=id)
+    story_ends = models.StoryEnd.objects.all().filter(genre=id)
     story_ends_list = list(story_ends)
     rand_number_three = random.randrange(0, len(story_ends_list))
     story_end = story_ends_list[rand_number_three]
@@ -69,7 +69,7 @@ def get_my_stories_page(request, id):
     print(request.user.id)
     if request.user.is_authenticated and str(request.user.id) == id:
 
-        story_ideas = StoryIdea.objects.all().filter(user = request.user.id)
+        story_ideas = models.StoryIdea.objects.all().filter(user = request.user.id)
             
         context = {
             'story_ideas': story_ideas
@@ -85,7 +85,7 @@ def get_my_stories_idea(request, id, idea_id):
     
     # code for editing story
     if request.user.is_authenticated and str(request.user.id) == id:
-        story_idea = StoryIdea.objects.get(id = idea_id)
+        story_idea = models.StoryIdea.objects.get(id = idea_id)
         if request.method == 'POST':
             form = StoryIdeaForm(request.POST, instance = story_idea)
             if form.is_valid():
@@ -93,7 +93,7 @@ def get_my_stories_idea(request, id, idea_id):
                 title = request.POST.get('title')
                 messages.add_message(request, messages.WARNING, f'Successfully edited story idea: {title}.')
                 # repeated elsewhere
-                story_ideas = StoryIdea.objects.all().filter(user = request.user.id)
+                story_ideas = models.StoryIdea.objects.all().filter(user = request.user.id)
             
                 context = {
                 'story_ideas': story_ideas
@@ -121,11 +121,11 @@ def get_my_stories_idea(request, id, idea_id):
 
 @login_required
 def delete_my_stories_idea(request, id, idea_id):
-    story_idea = get_object_or_404(StoryIdea, id = idea_id)
+    story_idea = get_object_or_404(models.StoryIdea, id = idea_id)
     title = story_idea.title
     story_idea.delete()
     messages.add_message(request, messages.ERROR, f'Successfully deleted story idea: {title}.')
-    story_ideas = StoryIdea.objects.all().filter(user = request.user.id)
+    story_ideas = models.StoryIdea.objects.all().filter(user = request.user.id)
             
     context = {
     'story_ideas': story_ideas
