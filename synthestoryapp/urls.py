ec09a81af128ea4a4cf4f61732b2edd5602ab8d4
CarlG96 <carl.gardiner1@hotmail.com>
2022-09-21 11:28:11 +0000

    docs: Add docstrings to urls and update in line with PEP8 guidance

--- synthestoryapp/urls.py
+++ synthestoryapp/urls.py
@@ -1,12 +1,16 @@
-from . import views
-from .views import get_genre_page, get_genre_type_page, get_home_page, get_my_stories_page, get_my_stories_idea, delete_my_stories_idea
+"""
+Code for Django url routing
+"""
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', get_home_page, name='home'),
-    path('genre-page/', get_genre_page, name='genre-page'),
-    path('genre-type/<str:id>/', get_genre_type_page, name='genre-type'),
-    path('my-stories/<str:id>/', get_my_stories_page, name='my-stories'),
-    path('my-stories/<str:id>/<str:idea_id>/', get_my_stories_idea, name='my-stories-idea'),
-    path('delete/<str:id>/<str:idea_id>/', delete_my_stories_idea, name='delete-stories-idea')
+    path('', views.get_home_page, name='home'),
+    path('genre-page/', views.get_genre_page, name='genre-page'),
+    path('genre-type/<str:id>/', views.get_genre_type_page, name='genre-type'),
+    path('my-stories/<str:id>/', views.get_my_stories_page, name='my-stories'),
+    path('my-stories/<str:id>/<str:idea_id>/', views.get_my_stories_idea,
+         name='my-stories-idea'),
+    path('delete/<str:id>/<str:idea_id>/', views.delete_my_stories_idea,
+         name='delete-stories-idea')
 ]
