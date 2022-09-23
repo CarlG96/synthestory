bf53884cebe36ce02b373a980acad20651f48919
CarlG96 <carl.gardiner1@hotmail.com>
2022-09-23 10:12:50 +0000

    test: Add automated test for views and reset database for validation

--- synthestoryapp/test_views.py
+++ synthestoryapp/test_views.py
@@ -5,4 +5,19 @@ from django.test import TestCase
 from synthestoryapp import views
 
 
+class TestViews(TestCase):
+
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
     
+    def test_get_genre_page(self):
+        response = self.client.get('/genre-page')
+        self.assertEqual(response.status_code, 200)
+
+    # def test_get_type_page(self):
+
+    # def test_get_my_stories_page(self):
+
+    # def test_get_my_story_idea_page(self):
