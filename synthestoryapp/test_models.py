1f80298ec6f1093f5eafde021ecb5f25ff4d1e90
CarlG96 <carl.gardiner1@hotmail.com>
2022-09-28 11:35:31 +0000

    test: Add automated tests for logged in views

--- synthestoryapp/test_models.py
+++ synthestoryapp/test_models.py
@@ -5,7 +5,12 @@ from django.test import TestCase
 from synthestoryapp import models
 
 
-# class TestModels(TestCase):
+class TestModels(TestCase):
 
-#     def test_genre_defaults_to_placeholder(self):
-#         genre = Gen
+    def test_genre_defaults_to_placeholder(self):
+        """
+        Function to test whether a Genre model created in the database 
+        without an image will default to 'placeholder'.
+        """
+        genre = models.Genre.objects.create()
+        self.assertTrue(genre.genre_image == 'placeholder')
--- synthestoryapp/test_views.py
+++ synthestoryapp/test_views.py
@@ -2,22 +2,52 @@
 Code for testing views in Django.
 """
 from django.test import TestCase
-from synthestoryapp import views
+from django.contrib.auth import get_user_model
+from synthestoryapp import views, models
 
 
-class TestViews(TestCase):
+class TestLoggedInViews(TestCase):
+
+    def setUp(self):
+        username = 'BobTheBuilder'
+        password = 'HelloWorld1234'
+        user = get_user_model()
+        self.user = user.objects.create_user(
+            username=username,
+            password=password
+        )
+        login = self.client.login(username=username, password=password)
+        self.assertTrue(login)
+
+        genre = models.Genre.objects.create(genre_title='testgenre')
+        story_start = models.StoryStart.objects.create(story_text='Hello World', genre=genre)
+        story_middle = models.StoryMiddle.objects.create(story_text='Hello World', genre=genre)
+        story_end = models.StoryEnd.objects.create(story_text='Hello World', genre=genre)
+        story_idea = models.StoryIdea.objects.create(title='Bob', story_text='TheBuilder', user=self.user)
 
     def test_get_home_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
     
-    # def test_get_genre_page(self):
-    #     response = self.client.get('/genre-page')
-    #     self.assertEqual(response.status_code, 200)
+    def test_get_genre_page(self):
+        response = self.client.get('/genre-page/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'genre-page.html')
 
-    # def test_get_type_page(self):
+    def test_get_genre_type_page(self):
+        response = self.client.get('/genre-type/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'genre-type.html')
 
-    # def test_get_my_stories_page(self):
+    def test_get_my_stories_page(self):
+        response = self.client.get('/my-stories/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'my-stories.html')
 
-    # def test_get_my_story_idea_page(self):
+    def test_get_my_story_idea_page(self):
+        response = self.client.get('/my-stories/1/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'story-idea.html')
+
+# class TestLoggedOutViews(TestCase):
