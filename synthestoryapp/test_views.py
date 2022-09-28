56d31e4d4c350380fa7b9049550f17fa38d894ef
CarlG96 <carl.gardiner1@hotmail.com>
2022-09-28 12:29:23 +0000

    test: Add tests for logged out user to be redirected to login page

--- synthestoryapp/test_views.py
+++ synthestoryapp/test_views.py
@@ -11,8 +11,7 @@ class TestLoggedInViews(TestCase):
     def setUp(self):
         username = 'BobTheBuilder'
         password = 'HelloWorld1234'
-        user = get_user_model()
-        self.user = user.objects.create_user(
+        self.user = get_user_model().objects.create_user(
             username=username,
             password=password
         )
@@ -23,7 +22,6 @@ class TestLoggedInViews(TestCase):
         story_start = models.StoryStart.objects.create(story_text='Hello World', genre=genre)
         story_middle = models.StoryMiddle.objects.create(story_text='Hello World', genre=genre)
         story_end = models.StoryEnd.objects.create(story_text='Hello World', genre=genre)
-        story_idea = models.StoryIdea.objects.create(title='Bob', story_text='TheBuilder', user=self.user)
 
     def test_get_home_page(self):
         response = self.client.get('/')
@@ -41,13 +39,58 @@ class TestLoggedInViews(TestCase):
         self.assertTemplateUsed(response, 'genre-type.html')
 
     def test_get_my_stories_page(self):
-        response = self.client.get('/my-stories/1/')
+        response = self.client.get(f'/my-stories/{self.user.id}/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'my-stories.html')
 
     def test_get_my_story_idea_page(self):
-        response = self.client.get('/my-stories/1/1/')
+        story_idea = models.StoryIdea.objects.create(title='Bob', story_text='TheBuilder', user=self.user)
+        response = self.client.get(f'/my-stories/{self.user.id}/{story_idea.id}/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'story-idea.html')
 
-# class TestLoggedOutViews(TestCase):
+    # def test_403_protection(self):
+    #     username = 'PatThePostman'
+    #     password = 'GoodbyeMoon1234'
+    #     print(self.user.id)
+    #     second_user = get_user_model().objects.create_user(
+    #         username=username,
+    #         password=password
+    #     )
+    #     story_idea = models.StoryIdea.objects.create(title='Pat', story_text='ThePostman', user=second_user)
+
+    #     response = self.client.get(f'/my-stories/{second_user.id}/')
+    #     self.assertEqual(response.status_code, 403)
+
+class TestLoggedOutViews(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='BobTheBuilder',
+            password='HelloWorld1234'
+        )
+        genre = models.Genre.objects.create(genre_title='testgenre')
+        story_start = models.StoryStart.objects.create(story_text='Hello World', genre=genre)
+        story_middle = models.StoryMiddle.objects.create(story_text='Hello World', genre=genre)
+        story_end = models.StoryEnd.objects.create(story_text='Hello World', genre=genre)
+
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_genre_page_redirect(self):
+        response = self.client.get('/genre-page/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_genre_type_page_redirect(self):
+        response = self.client.get('/genre-type/1/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_my_stories_page_redirect(self):
+        response = self.client.get(f'/my-stories/{self.user.id}/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_story_idea_redirect(self):
+        story_idea = models.StoryIdea.objects.create(title='Bob', story_text='TheBuilder', user=self.user)
+        response = self.client.get(f'/my-stories/{self.user.id}/{story_idea.id}/')
+        self.assertEqual(response.status_code, 302)
